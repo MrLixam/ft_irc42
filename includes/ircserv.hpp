16016b00edd65bb504bf654f500789463bd4264c
Sandoct <gpouzet42@gmail.com>
2024-06-10 16:24:31 +0200

    structured message

--- includes/ircserv.hpp
+++ includes/ircserv.hpp
@@ -1,7 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ircserv.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: r <marvin@42.fr>                           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/10 16:23:28 by r                 #+#    #+#             */
+/*   Updated: 2024/06/10 16:23:30 by r                ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef IRCSERV_HPP
 #define IRCSERV_HPP
 
 #define MAX_CLIENTS 1024
+
+typedef struct structured_message
+{
+	std::string				nickname;
+	std::string				user;
+	std::string				host;
+	std::string				command;
+	std::list<std::string>	params;
+}							struct_msg;
 
 int	parseArgs(char **argv);
 
--- src/parsing.cpp
+++ src/parsing.cpp
@@ -6,7 +6,7 @@
 /*   By: lvincent <lvincent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 17:27:35 by lvincent          #+#    #+#             */
-/*   Updated: 2024/06/08 10:51:51 by lvincent         ###   ########.fr       */
+/*   Updated: 2024/06/10 16:05:32 by r                ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,44 @@
 #include <string>
 #include <cerrno>
 #include <iostream>
+
+stm	structuring_message(std::string message)
+{
+	struct_msg			msg;
+    std::istringstream	iss(message);
+    std::string			token;
+
+    // Parse prefix
+    if (message[0] == ':')
+	{
+        iss.ignore(1); // ignore the initial ':'
+        std::getline(iss, token, ' ');
+        size_t exclam = token.find('!');
+        size_t at = token.find('@');
+        if (exclam != std::string::npos)
+		{
+            msg.nickname = token.substr(0, exclam);
+            msg.user = token.substr(exclam + 1, at - exclam - 1);
+            msg.host = token.substr(at + 1);
+        }
+		else if (at != std::string::npos)
+		{
+            msg.nickname = token.substr(0, at);
+            msg.host = token.substr(at + 1);
+        }
+		else
+            msg.nickname = token;
+    }
+
+    // Parse command
+    iss >> msg.command;
+
+    // Parse params
+    while (iss >> token)
+        msg.params.push_back(token);
+
+    return msg;
+}
 
 int parseArgs(char **argv)
 {
