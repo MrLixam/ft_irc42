f6d158e24d0e06170881791ce74e89a867c53579
Liam VINCENT <lixam.vincent@gmail.com>
2024-06-14 14:20:33 +0200

    added some macros and an error message generator

--- includes/ircserv.hpp
+++ includes/ircserv.hpp
@@ -6,7 +6,7 @@
 /*   By: lvincent <lvincent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 16:23:28 by r                 #+#    #+#             */
-/*   Updated: 2024/06/13 14:53:35 by r                ###   ########.fr       */
+/*   Updated: 2024/06/14 14:19:47 by lvincent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,26 @@
 #include <list>
 #include <string>
 
-#define MAX_CLIENTS 1024
+#define	MAX_CLIENTS				1024
+
+#define	ERR_NOSUCHNICK			401
+#define	ERR_NOSUCHCHANNEL		403
+#define	ERR_CANNOTSENDTOCHAN	404
+#define	ERR_TOOMANYTARGETS		407
+#define	ERR_NOTEXTTOSEND 		412
+#define	ERR_UNKNOWNCOMMAND		421
+#define	ERR_NONICKNAMEGIVEN		431
+#define	ERR_ERRONEUSNICKNAME	432
+#define	ERR_NICKNAMEINUSE		433
+#define	ERR_USERNOTINCHANNEL	441
+#define	ERR_NOTONCHANNEL		442
+#define	ERR_USERONCHANNEL		443
+#define	ERR_NOTREGISTERED		451
+#define	ERR_NEEDMOREPARAMS		461
+#define	ERR_ALREADYREGISTRED	462
+#define	ERR_PASSWDMISMATCH		464
+#define	ERR_YOUREBANNEDCREEP	465
+#define	ERR_YOUWILLBEBANNED		466
 
 typedef struct structured_message
 {
--- src/ServerUtils.cpp
+++ src/ServerUtils.cpp
@@ -3,14 +3,59 @@
 /*                                                        :::      ::::::::   */
 /*   ServerUtils.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: r <marvin@42.fr>                           +#+  +:+       +#+        */
+/*   By: lvincent <lvincent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 15:29:36 by r                 #+#    #+#             */
-/*   Updated: 2024/06/13 15:56:44 by r                ###   ########.fr       */
+/*   Updated: 2024/06/14 14:19:43 by lvincent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Server.hpp"
+
+std::string getErrorMessage(std::string errorInfo = "", int errorCode)
+{
+	switch (errorCode)
+	{
+		case 401:
+			return ("\"" + errorInfo + " :No such nick/channel\"");
+		case 403:
+			return ("\"" + errorInfo + " :No such channel\"");
+		case 404:
+			return ("\"" + errorInfo + " :Cannot send to channel\"");
+		case 407:
+			return ("\"" + errorInfo + " :407 recipients. Message not delivered\"");
+		case 412:
+			return ("\":No text to send\"");
+		case 421:
+			return ("\"" + errorInfo + " :Unknown command\"");
+		case 431:
+			return ("\":No nickname given\"");
+		case 432:
+			return ("\"" + errorInfo + " :Erroneous nickname\"");
+		case 433:
+			return ("\"" + errorInfo + " :Nickname is already in use\"");
+		case 441:
+			return ("\"" + errorInfo + " :They aren't on that channel\"");
+		case 442:
+			return ("\"" + errorInfo + " :You're not on that channel\"");
+		case 443:
+			return ("\"" + errorInfo + " :is already on channel\"");
+		case 451:
+			return ("\":You have not registered\"");
+		case 461:
+			return ("\"" + errorInfo + " :Not enough parameters\"");
+		case 462:
+			return ("\":Unauthorized command (already registered)\"");
+		case 464:
+			return ("\":Password incorrect\"");
+		case 465:
+			return ("\":You are banned from this server\"");
+		case 466:
+			return ("");
+		default:
+			return ("");
+	}
+}
 
 bool Server::usernameExists(const std::string username, int fd) const
 {
