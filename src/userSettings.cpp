31c921563cc60562d09b3d8fde4c8242e7ded889
Liam VINCENT <lixam.vincent@gmail.com>
2024-07-02 14:48:52 +0200

    fixed a bug in the quit command making users unable to disconnect when they left the only channel in a server and it got deleted

--- src/userSettings.cpp
+++ src/userSettings.cpp
@@ -6,7 +6,7 @@
 /*   By: lvincent <lvincent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 14:27:35 by r                 #+#    #+#             */
-/*   Updated: 2024/07/01 19:58:14 by lvincent         ###   ########.fr       */
+/*   Updated: 2024/07/02 14:45:10 by lvincent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,8 +96,13 @@ void	Server::command_quit(struct_msg msg, int fd)
 		it->second.getCl().erase(fd);
 		it->second.getOp().erase(fd);
 		if (!it->second.getCl().size())
+		{
 			this->_channels.erase(it++);
+			if (it == this->_channels.end())
+				break ;
+		}
 	}
+	
 	messageToClient(fd, QUIT_RPL(user_id(myClient.getNickname(), myClient.getUsername()), sendoff));
 	myClient.setDisconnect(true);
 }
